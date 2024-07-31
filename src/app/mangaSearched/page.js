a1e484a01b36ed95d44a9cf63278037248a88880
FaresAB-git <fares.ab74pgmail.com>
2024-07-31 17:19:46 +0200

    test

--- src/app/mangaSearched/page.js
+++ src/app/mangaSearched/page.js
@@ -1,32 +1,46 @@
-// app/mangaSearched/page.js
 'use client';
-import { useRouter, useSearchParams } from 'next/navigation';
 import { useState, useEffect, Suspense } from 'react';
+import { useSearchParams } from 'next/navigation'; // Importez useSearchParams
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import Gallery from '../components/Gallery';
 import styles from '../styeComponents/searchBar.module.css';
 
 export default function MangaSearched() {
-    const [searchResults, setSearchResults] = useState([]);
     const searchParams = useSearchParams();
-    const query = searchParams.get('query');
+    const [searchResults, setSearchResults] = useState([]);
+    const [loading, setLoading] = useState(true); 
+    const query = searchParams.get('manga'); 
 
     useEffect(() => {
         if (query) {
-            const results = localStorage.getItem('searchResults');
-            if (results) {
-                setSearchResults(JSON.parse(results));
-            }
+            const fetchResults = async () => {
+                // Remplacez l'utilisation du localStorage par un appel API si possible
+                const results = localStorage.getItem('searchResults');
+                if (results) {
+                    setSearchResults(JSON.parse(results));
+                }
+                setLoading(false); // Marquer le chargement comme terminé
+            };
+
+            fetchResults();
         }
-    }, [query]); // Surveillez les changements de query
+    }, [query]); // Dépendre du paramètre de query
+
+    if (loading) {
+        return <div>Loading...</div>; // Afficher un message de chargement pendant la récupération des résultats
+    }
 
     return (
-        <Suspense>
+        <>
             <Header />
-            <SearchBar />
-            <div className={styles.scanIconContainer}></div>
+            <Suspense>
+                <SearchBar />
+            </Suspense>
+            <div className={styles.scanIconContainer}>
+                {/* Votre contenu ici */}
+            </div>
             <Gallery mangaList={searchResults} />
-        </Suspense>
+        </>
     );
 }
