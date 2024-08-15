9ba7a0e43387aecda1fec43240553115acd1a5ad
FaresAB-git <fares.ab74pgmail.com>
2024-08-16 00:09:34 +0200

    responsice test

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -43,14 +43,8 @@ export default function Carousel({ popularManga }) {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
             }
+           
         ]
     };
     
